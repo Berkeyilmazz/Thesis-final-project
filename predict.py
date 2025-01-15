a43986443a1dcc8161a6bb13c44df3b966da9bf4
Berkeyilmazz <97527052+Berkeyilmazz@users.noreply.github.com>
2025-01-15 20:36:45 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Load the saved model
 model = joblib.load('final_model.pkl')
 
-df = pd.read_csv('actual_outcomes.csv')
+df = pd.read_csv('eve_data.csv')
 
 # Ensure that the columns match the training features
 try:
